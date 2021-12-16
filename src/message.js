1dbcb5a21722d75bd4efd8aee333488eb0ee38be
henriqueyun <henriqueyun@gmail.com>
2021-12-16 06:53:51 -0300

    Ops, faltaram os modelos do  sequelize, alterados para inglês

---
+++ src/message.js
@@ -0,0 +1,44 @@
+const { Sequelize, DataTypes } = require('sequelize')
+
+const Scheduling = require('./scheduling')
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+)
+
+const Message = sequelize.define('message', {
+    sender: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    sendTime: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    xetId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Agendamento,
+        key: 'id'
+      }
+    }
+  }, { timestamps: true });
+
+Scheduling.hasMany(Message, 
+  { sourceKey: 'id', foreignKey: 'xetId' });
+(async () => {
+  await sequelize.sync()
+})()
+
+module.exports = Message
---
+++ src/scheduling.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes } = require('sequelize')
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+)
+
+const Scheduling = sequelize.define('scheduling', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  owner: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  startTime: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  endTime: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, { timestamps: true });
+
+  (async () => {
+    await sequelize.sync()
+  })()
+
+module.exports = Scheduling
