46f4da9f48a6c86696156bae1c9d3889fc863f84
henriqueyun <henriqueyun@gmail.com>
2021-12-16 06:53:00 -0300

    Adicionando linter e normalizando idioma utilizado no código para inglês (apesar dos commits estarem em portguês)

--- src/index.js
+++ src/index.js
@@ -1,66 +1,76 @@
-const port = process.env.port
-const Agendamento = require('./agendamento')
-const Mensagem = require('./mensagem')
-
+const logger = require('pino')()
 const restify = require('restify')
+const server = restify.createServer()
 const corsMiddleware = require('restify-cors-middleware')
-
-
 const cors = corsMiddleware({
   origins: ['http://localhost:8080']
 })
-
-const server = restify.createServer()
-
-server.use(restify.plugins.bodyParser())
 server.pre(cors.preflight)
 server.use(cors.actual)
+server.use(restify.plugins.bodyParser())
+
+const Scheduling = require('./scheduling')
+const Message = require('./mesasge')
+const APIErrors = {
+  general: { message: 'Error: ' + JSON.stringify(err) },
+  missingBodyParams: { message: 'Error: ' + JSON.stringify(err) }
+}
 
 server.post('/xet', async (req, res) => {
-  if ('owner', 'title', 'startTime', 'endTime' in req.body) {
-    const agendamento = req.body
-    Agendamento.create(agendamento)
+  const scheduling = req.body
+  logger.debug(`/xet ${scheduling}`)
+  if ('owner', 'title', 'startTime', 'endTime' in scheduling) {
+    Scheduling.create(scheduling)
       .then(() => {
+        logger.debug(`/xet ${scheduling} created`)
         res.send(200)
       }).catch(err => {
-        res.send(500, { message: 'Server Internal Error: ' + JSON.stringify(err) })
+        logger.error(`/xet ${scheduling} ${err.message}`)
+        res.send(500, APIErrors.general + err.message)
       })
   } else {
-    res.send(400, { message: 'Missing Body Params' })
+    res.send(400, APIErrors.missingBodyParams)
   }
 })
 
 server.get('/xet/all', async (req, res) => {
-    Agendamento.findAll()
-      .then(data => {
-        res.send(data)
-      }).catch(err => {
-        res.send(500, { message: 'Server Internal Error' + JSON.stringify(err) })
-      })
+  logger.debug(`/xet/all`)
+  Scheduling.findAll()
+    .then(xets => {
+      logger.debug(`/xet/all return ${xets}`)
+      res.send(200, xets)
+    }).catch(err => {
+      logger.error(`/xet/all ${err.message}`)
+      res.send(500, APIErrors.general + err.message)
+    })
 })
 
 server.get('/xet/:id/message/all', async (req, res) => {
-  Mensagem.findAll({ where: { xetId: req.params.id }})
-    .then((mensagens) => {
-      res.json(mensagens)
+  logger.debug(`/xet/:id/message/all`)
+  Message.findAll({ where: { xetId: req.params.id }})
+    .then((messages) => {
+      logger.debug(`/xet/:id/message/all return ${messages}`)
+      res.send(200, messages)
     }).catch(err => {
-      console.error(err)
-      res.send(500, { message: 'Server Internal Error: ' + err})
+      logger.error(`/xet/:id/message/all ${err.message}`)
+      res.send(500, { message: APIErrors.general + err.message })
     })
 })
-
+ 
 server.post('/xet/:id/message', async (req, res) => {
-  const mensagem = req.body
-  mensagem.xetId = req.params.id
-  Mensagem.create(mensagem)
+  logger.debug(`/xet/:id/message`)
+  const message = req.body
+  message.xetId = req.params.id
+  Message.create(message)
     .then(() => {
+      logger.debug(`/xet/:id/message ${scheduling}`)
       res.status(200)
     }).catch(err => {
-      console.error(err)
-      res.send(500, { message: 'Server Internal Error: ' + JSON.stringify(err) })
+      logger.error(`/xet/:id/message message body: ${JSON.stringify(message)} ${err.message}`)
+      res.send(500, { message: '' + JSON.stringify(err) })
     })
 })
 
-server.listen(port, () => { console.log('Server running on ' + port) })
+server.listen(process.env.port, () => { logger.info(`xet-backend server running on ${process.env.port}`) })
 
 module.exports = server
